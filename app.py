33e7989530820cef0ff36f1016dc1fa6bebd1aa8
Thomas Benzshawel <benzshawelt@msoe.edu>
2025-02-12 19:11:27 -0600

    Updated flask

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello MSOE'
+    return 'Hello MSOE, my name is Thomas Benzshawel! Don't click this link: https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
