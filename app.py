14fed9dfde487b3746b91ff6ff5cc3573d289347
Thomas Benzshawel <benzshawelt@msoe.edu>
2025-02-12 19:14:38 -0600

    Updated flask again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello MSOE, my name is Thomas Benzshawel! Don't click this link: https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    return 'Hello MSOE, my name is Thomas Benzshawel! Don not click this link: https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 
 if __name__ == '__main__':
